8de2db763268781fb6b6f0a82f42ecfde7029dfd
officialsanju2004 <godsanju21@gmail.com>
2026-03-06 11:07:12 +0530

    Update UserEnquiryFolder.js

--- server/App/Models/Middleware/Routes/controllers/web/UserEnquiryFolder.js
+++ server/App/Models/Middleware/Routes/controllers/web/UserEnquiryFolder.js
@@ -1,35 +1,75 @@
 const enquiryModel = require("../../../../EnquiryModel");
 
-let enquiryInsert= (req, res) => {
-    let { name, email,  message } = req.body; //data from api
-    
-    let enquiry = new enquiryModel({
-      name,
-      email,
- 
-      message 
+const nodemailer = require("nodemailer");
+let enquiryInsert = async (req, res) => {
+  let { name, email, message } = req.body; //data from api
+
+
+  try {
+
+    const transporter = nodemailer.createTransport({
+      service: "gmail",
+      auth: {
+        user: "godsanju21@gmail.com",
+
+        pass: "jlmi qchi xcyl afsy", //app password
+      },
     });
-    
-    enquiry
-      .save()
-      .then(() => {
-        res.send({ status: 1, mess: "Data Saved Sucessfully!" });
-      })
-      .catch((err) => {
-        res.send({ status: 0, mess: "Error While saving Data!", error: err });
-      });
+
+    const mailOptions = {
+      from: email,
+      to: "godsanju21@gmail.com",
+      subject: `New Contact Form Submission`,
+      html: `
+         <h3>New Message from Portfolio Website </h3>
+         <p><b>Name :</b> ${name}</p>
+         <p><b>Email :</b> ${email}</p>
+         <p><b>Message:</b> ${message}</p>
+         `
+    };
+    setImmediate(() => {
+      transporter
+        .sendMail(mailOptions)
+        .then(() => console.log("Message sent to :", email))
+        .catch((err) => {
+          console.error(" email error:", err);
+        });
+    })
+
+
+
+  } catch (e) {
+    console.log(e);
+
   }
 
-let enquiryDelete= async (req, res) => {
-    let enquiryId = req.params.id;
-    let enquiryUpdate = await enquiryModel.deleteOne({ _id: enquiryId });
-    res.send({ status: 1, mess: "enquiry deleted" });
-  }
+  let enquiry = new enquiryModel({
+    name,
+    email,
 
-let enquiryView= async (req, res) => {
-    let enquiry = await enquiryModel.find();
-    res.status(200).json({ status: 1, mess: "Enquiry List", enquiryList : enquiry});
-  }
+    message
+  });
+
+  enquiry
+    .save()
+    .then(() => {
+      res.send({ status: 1, mess: "Data Saved Sucessfully!" });
+    })
+    .catch((err) => {
+      res.send({ status: 0, mess: "Error While saving Data!", error: err });
+    });
+}
+
+let enquiryDelete = async (req, res) => {
+  let enquiryId = req.params.id;
+  let enquiryUpdate = await enquiryModel.deleteOne({ _id: enquiryId });
+  res.send({ status: 1, mess: "enquiry deleted" });
+}
+
+let enquiryView = async (req, res) => {
+  let enquiry = await enquiryModel.find();
+  res.status(200).json({ status: 1, mess: "Enquiry List", enquiryList: enquiry });
+}
 
 
-  module.exports={enquiryInsert,enquiryDelete,enquiryView};
+module.exports = { enquiryInsert, enquiryDelete, enquiryView };
